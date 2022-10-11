fc47e069275a4cb41a4c9e61c2bce86454f1a428
maxi87luc <106201783+maxi87luc@users.noreply.github.com>
2022-10-11 17:05:22 -0300

    lista de precios

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -12,26 +12,38 @@ import Contexts from '../Context/Contexts'
 
 
 function ItemDetail ({modelo}) {
+    console.log(modelo[0])
+    console.log(modelo[1])
+
+    
+
+   
 
     const context = useContext(Contexts.cartContext)
     
     const [pedido, setPedido] = useState([])
     const [state, setState] = useState(0)
-    const [nombreModelo] = useState(modelo.id)
+    const [nombreModelo] = useState(modelo[0].id)
 
+    console.log(nombreModelo)
+    console.log(pedido)
+    
     
 
     
 
     
 
-    const talles = modelo.talles.filter(talle=>talle!=="")
+    const talles = modelo[0].talles.filter(talle=>talle!=="")
     const suelasPNG = require.context('../multimedia/images/suelasPNG') 
-    const urlImage1 = suelasPNG(`./${ modelo.id }1.png`)
-    const urlImage2 = suelasPNG(`./${ modelo.id }2.png`)  
+    const urlImage1 = suelasPNG(`./${ nombreModelo }1.png`)
+    const urlImage2 = suelasPNG(`./${ nombreModelo }2.png`)  
       
-        
-   
+    let totalPares   
+    let suma = pedido.forEach((e)=>{
+         totalPares = totalPares + e.cantidad
+         return totalPares
+    })
     const sumarItem = (talle, cantidad) => {        
 
         
@@ -42,8 +54,12 @@ function ItemDetail ({modelo}) {
             const itemFind = pedido.find(o => o.talle === newItem.talle )
             itemFind.cantidad = cantidad
             
+            
+
+            
         } else {
             setPedido([...pedido, newItem])
+            
         }
     } 
 
@@ -59,20 +75,26 @@ function ItemDetail ({modelo}) {
                     <h2 className="row centrar">{modelo.id}</h2>  
                            
                     <p className="row centrar">                  
-                    Material: {modelo.material}
+                        Material: {modelo.material}
                     </p>
                     <p className="row centrar">Curva: {modelo.genero} </p>
                      
+                    
+                    
                                    
                     <div>
                         
                         <ul className="tallesList container">            
                             {talles.map((talle)=><li className={state===0?"row talleList ":"d-none"}><p className="col-4 talleItemCount">{talle}</p> <ItemCount stock={10} onAdd={sumarItem} initial={0} talle={talle} /></li>)}
                         </ul> 
+                        <center>
+                        <h3>${modelo[1].precio}</h3>
+                        <h3>${suma*modelo[1].precio}</h3>
+                        
                         <button onClick={()=>context.addToCart(nombreModelo, pedido, setState)} className={state===0?"btn btn-primary":"d-none"}>Agregar a carrito</button>  
                         <NavLink to={"/cart"}><button  className={state===1?"btn btn-primary":"d-none"}>Comprar</button></NavLink>
                         <h3 className={state===2?"":"d-none"}>Este articulo ya esta en tu carrito</h3>
-                        
+                        </center>
                                                     
                         
                     </div>
--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,26 +1,42 @@
 
 import {fetchItemData} from '../data/items';
-import {useState , useEffect} from 'react'
+import {useState , useEffect} from 'react';
 import { useParams } from 'react-router-dom';
 import ItemDetail from './ItemDetail';
 import NoHayElementos from './NoHayElementos';
+import fetchPriceData from '../data/precios';
 
 function ItemDetailContainer () {
    
     const [state, setState] = useState()
+    const [price, setPrice] = useState()
+    
 
     const {id} = useParams()
+    console.log(id)
     
     useEffect(()=>{
         fetchItemData(id).then((value)=>{ 
-            setState(value)
+            setState(value);        
+           
+
+
         })
+        fetchPriceData(id).then((value)=>{ 
+            setPrice(value)
+                
+            })
+        console.log("antes de pricedata: "+ state)
+        
     })
+   
+    console.log(state)
+    console.log(price)
+    
    
     return (
         <div className="ItemDetailContainer d-grid  ">
-            {state?<ItemDetail modelo={state}/>:<NoHayElementos/>}
-            
+            {state?<ItemDetail modelo={[state,price]}/>:<NoHayElementos/>}          
             
             
         </div> 
