17488842334967e2f67c70fd07d3e17740a55fec
Maximiliano Lucero <maxi87luc@gmail.com>
2022-10-11 22:19:44 -0300

    mejorar Cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,14 +7,19 @@ export default function Cart (){
     const context = useContext(Contexts.cartContext)
     
     let items = context.state
+    let totalCarrito = 0
     console.log(items)
     if(items.length){
         items.forEach((obj)=>{
             obj.curva.sort((a, b)=>{
             return a.talle - b.talle
             })
-        })        
-    }    
+        })
+        items.forEach((obj)=>{
+            totalCarrito = totalCarrito + obj.total
+        } )       
+    }   
+    console.log(totalCarrito) 
     return (
         <div className="cartContainer">
             <div className="cart container">
@@ -31,12 +36,17 @@ export default function Cart (){
                             </tr>
                         </table>
                         <img src={BotonQuitar} id={item.modelo} type="button" onClick={()=>context.deleteItem(item.modelo)} className="BotonQuitar col-1" alt=""/>
-                        <h4 className="col-2">${item.total}</h4>
+                        <p className="col-2"><strong>${item.total}</strong> +IVA</p>
                     
                     
                     </li>)):<p>No hay elementos</p>}
                 </ul>
-                <button onClick={()=>context.removeList()} className="btn btn-primary">Vaciar Carrito</button>
+                <div className="d-flex justify-content-between">
+                    <button onClick={()=>context.removeList()} className="btn btn-secondary">Vaciar Carrito</button>
+                    <h3><strong>Total: ${totalCarrito} +IVA</strong></h3>
+                    <button  className="btn btn-primary">Finalizar Compra</button>
+                   
+                </div>
             </div>
         </div> 
     )
